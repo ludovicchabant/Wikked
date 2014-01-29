38c7dd0cad2a0d0c407af8811e9badaae1765bd4
Ludovic Chabant <ludovic@chabant.com>
2014-01-28 21:45:10 -0800

    Setup logging in WSGI app helper.

--- wikked/wsgiutil.py
+++ wikked/wsgiutil.py
@@ -1,11 +1,21 @@
+import os
+import sys
+import logging
+import logging.handlers
+from wikked.wiki import WikiParameters
 
-def get_wsgi_app(wiki_root):
-    import os
+
+def get_wsgi_app(wiki_root, log_file=None):
     os.chdir(wiki_root)
+    logging.basicConfig(stream=sys.stderr)
 
     from wikked.web import app
-    from wikked.wiki import WikiParameters
     app.wiki_params = WikiParameters(wiki_root)
+
+    if log_file is not None:
+        h = logging.handlers.RotatingFileHandler(log_file, maxBytes=4096)
+        h.setLevel(logging.WARNING)
+        app.logger.addHandler(h)
 
     return app
 
