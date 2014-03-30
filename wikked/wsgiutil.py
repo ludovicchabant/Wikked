7a2132cfff0524bd5cefc579a4561e492c884955
Ludovic Chabant <ludovic@chabant.com>
2014-03-29 22:50:09 -0700

    Enable async update by default when using a WSGI application.

--- wikked/wsgiutil.py
+++ wikked/wsgiutil.py
@@ -5,9 +5,13 @@ import logging.handlers
 from wikked.wiki import WikiParameters
 
 
-def get_wsgi_app(wiki_root, log_file=None):
+def get_wsgi_app(wiki_root, log_file=None, async_update=True):
     os.chdir(wiki_root)
     logging.basicConfig(stream=sys.stderr)
+
+    if async_update:
+        import wikked.settings
+        wikked.settings.WIKI_ASYNC_UPDATE = True
 
     from wikked.web import app
     app.set_wiki_params(WikiParameters(wiki_root))
