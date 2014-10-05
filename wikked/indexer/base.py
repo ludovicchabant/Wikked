6284f1536a8a6bd27b08a3078cbc961eba5c8c82
Ludovic Chabant <ludovic@chabant.com>
2014-10-04 21:05:39 -0700

    Some doc strings.

--- wikked/indexer/base.py
+++ wikked/indexer/base.py
@@ -8,25 +8,34 @@ class HitResult(object):
 
 
 class WikiIndex(object):
-    def __init__(self):
-        pass
-
+    """ The search index for the wiki, allowing the user to run queries
+        to find pages.
+    """
     def start(self, wiki):
+        """ Called when the wiki is started. """
         pass
 
     def init(self, wiki):
+        """ Called when a new wiki is created. """
         pass
 
     def postInit(self):
+        """ Called after a new wiki was created. """
         pass
 
     def reset(self, pages):
+        """ Called when the index should be re-created from scratch
+            based on the given pages. """
         pass
 
     def updatePage(self, page):
+        """ Called when the entry for the given page should be updated
+            with the latest information. """
         pass
 
     def updateAll(self, pages):
+        """ Called when the entries for the given pages should be
+            updated with the latest information. """
         pass
 
     def search(self, query):
