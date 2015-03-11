f3b1e7f1b310719363143b5d049e39cbc374d249
Ludovic Chabant <ludovic@chabant.com>
2015-03-11 14:42:51 -0700

    wsgi: Default to a small-ish rotating log file handler.

--- wikked/wsgiutil.py
+++ wikked/wsgiutil.py
@@ -6,11 +6,11 @@ logger = logging.getLogger()
 
 
 def get_wsgi_app(wiki_root=None, async_update=False, log_file=None,
-        max_log_bytes=0, log_backup_count=0, log_level=logging.INFO):
+        max_log_bytes=4096, log_backup_count=0, log_level=logging.INFO):
     if log_file:
         from logging.handlers import RotatingFileHandler
         handler = RotatingFileHandler(log_file, maxBytes=max_log_bytes,
-                backupCount=log_backup_count)
+                                      backupCount=log_backup_count)
         handler.setLevel(log_level)
         logging.getLogger().addHandler(handler)
 
