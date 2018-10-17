2e3f557a4f5fcda592b1bb8a962fc8ac96c84310
Ludovic Chabant <ludovic@chabant.com>
2018-10-16 22:08:51 -0700

    docs: Remove documentation, it's now on a wiki at `wikked.bolt80.com`.

--- docs/assets/js/wikked/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#wiki-menu-shortcut').click(function() {
-        $('.wrapper').toggleClass('wiki-menu-active');
-        $('#wiki-menu').toggleClass('wiki-menu-active');
-    });
-});
-
